fba9477a4c2fb6621de1100f1c5393b5c9aa49dc
HamzaMhirsi <42467250+HamzaMhirsi@users.noreply.github.com>
2019-03-14 12:13:20 +0100

    Balcklisted IP
    In this file we will search on our Threat Intel if the captured IP is blacklisted or not, you may adapt this file to our code

---
+++ Iptriage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import mmap
+
+"""
+The Ip is the field reiceved in the frame we will need to scan the external IP 
+"""
+
+file = open("reputation.data")
+IP ='207.241.231.146'
+s = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
+if s.find(IP) != -1:
+	print "This "+IP+" is blacklisted"
+file.close()
